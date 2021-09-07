998f7da75d2bd869a5dcf9fe5c2111ecc8b5eabc
LEE MYEONG JIN <71704247+NoirNorie@users.noreply.github.com>
2021-09-07 17:59:26 +0900

    Add files via upload

---
+++ 2750.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 문제 번호: 2750(수 정렬하기)
+
+int main()
+{
+	vector<int> v;
+	int N; cin >> N;
+
+	for(int i = 0 ; i < N; i ++)
+	{
+		int x; cin >> x;
+		v.push_back(x);
+	}
+
+	for (int i = 0; i < v.size() -1; i++)
+	{
+		for (int j = i+1; j < v.size(); j++)
+		{
+			if (v[i] > v[j])
+			{
+				int tmp = v[j];
+				v[j] = v[i];
+				v[i] = tmp;
+			}
+		}
+	}
+
+	for (int i = 0; i < v.size() ;i++)
+	{
+		cout << v[i] << endl;
+	}
+	
+	return 0;
+}
