a01c254e3a0dee19a0b5b0ec3e9041ac956863da
LEE MYEONG JIN <71704247+NoirNorie@users.noreply.github.com>
2021-09-27 14:01:39 +0900

    Add files via upload
    BOJ - Q(3036): 링
    - 정수론 예제

---
+++ 3036.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 문제 번호: 3036(링)
+
+int gcd(int x, int y)
+{
+	while (y != 0)
+	{
+		int n = x % y;
+		x = y;
+		y = n;
+	}
+	return x;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N; cin >> N;
+
+	vector<int> v(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> v[i];
+	}
+
+	int front = v[0];
+
+	for (int i = 1; i < v.size(); i++)
+	{
+		int bot = gcd(front, v[i]);
+		cout << front / bot << "/" << v[i] / bot << '\n';
+	}
+
+	return 0;
+}
